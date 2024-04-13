dec412d6a722e5e960030ead656dc38591256b57
Rabi-anando-sarkar <rabianandos2002@gmail.com>
2024-04-13 22:48:00 +0530

    code edit

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -2,7 +2,6 @@ import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 import bcrypt from 'bcryptjs'
 import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
-import { send } from "process";
 
 export async function POST(request: Request){
     await dbConnect()
